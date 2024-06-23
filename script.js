2b37586dfa02c47950157bfd33ec85304b1281b9
Marvellous-cloud <marvelfemzy@gmail.com>
2024-06-23 22:02:08 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const header = document.querySelector('header');
+    header.style.opacity = 1;
+    header.style.transition = 'opacity 1s ease-in-out';
+});
